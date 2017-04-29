a1e49e982c5604cb8fda8344f0457b76e68c5d45
Jeff Doan <jeffdoan@jeffdoan.fios-router.home>
2017-04-29 11:13:45 -0700

    Adjust for mobiles & add in blur support

--- app.js
+++ app.js
@@ -1,36 +1,52 @@
 var html = document.getElementsByTagName('html')[0],
 	body = document.getElementsByTagName('body')[0],
+	// modal intro stuff
 	blContainer = document.getElementById('bl-container'),
 	blIntroContent = document.getElementById('bl-intro-content'),
 	blTitleLink = document.getElementById('bl-intro-button'),
+	// form stuff
 	blForm = document.getElementById('bl-form'),
 	blModalContainer = document.getElementById('bl-modal-wrapper-container'),
 	blModal = document.getElementById('bl-modal-wrapper'),
 	blModalFormContainer = document.getElementById('bl-modal-form'),
-	blModalSuccessImg = document.getElementById('bl-submitted-image'),
-	blImgSrc = blModalSuccessImg.getAttribute('data-src'),
-	blMobileModalSuccessImg = document.getElementById('bl-mobile-img'),
-	blMobileModalSuccessContent = document.getElementById('bl-submitted-content-wrapper'),
-	blModalCloseBtn = document.getElementById('bl-modal-close'),
 	blSubmit = document.getElementById('bl-submit'),
 	blEmail = document.getElementById('bl-email'),
 	blLabel = document.getElementById('bl-label'),
 	blError = document.getElementById('bl-form-microcopy-error'),
 	blEmailErrorMsg = 'Please enter a valid email.',
+	// success stuff
+	blModalSuccessImg = document.getElementById('bl-submitted-image'),
+	blImgSrc = blModalSuccessImg.getAttribute('data-src'),
+	blMobileModalSuccessImg = document.getElementById('bl-mobile-img'),
+	blMobileModalSuccessContent = document.getElementById('bl-submitted-content-wrapper'),
 	blSubittedContent = document.getElementById('bl-submitted-content'),
+	// learn more stuff
+	blLearnMoreContent = document.getElementById('bl-learn-more'),
+	blLearnMoreLink = document.getElementById('bl-learn-more-link'),
+	blLearnMoreBackButton = document.getElementById('bl-back-link'),
+	blLearnMoreVideo = document.getElementById('bl-video-holder'),
+	// close modal stuff
+	blModalCloseBtn = document.getElementById('bl-modal-close'),
 	blSubmittedDismiss = document.getElementById('bl-submitted-dismiss'),
+	// misc
 	hasContentClass = 'has-content',
 	narrowWindow = window.innerWidth <= 500 ? true : false,
-	scrollPos;
+	scrollPos,
+	modalContent = '<div id="bl-modal-wrapper-container"><div id="bl-modal-wrapper"><div id="bl-modal-form"><div id="bl-intro-content"><div id="bl-modal-title"><div id="bl-logo"></div>Help us improve{productName}.<small id="bl-modal-title-micro">Take part in beta tests &amp; user tests to make{productName}better.</small></div><form action="POST" id="bl-form" class="is-shown"><fieldset id="bl-form-wrapper"><label for="bl-email" id="bl-label-title">Enter your email</label><label id="bl-label" for="bl-email"><input type="email" id="bl-email" name="bl-email" value=""/></label><button id="bl-submit">Join</button></fieldset><small id="bl-form-microcopy-error"></small><small id="bl-form-microcopy">After joining, you&rsquo;re in control with what you help out with. Unsubscribe anytime you want.</small></form></div><div id="bl-submitted-content"> <img data-src="congrats.gif" id="bl-submitted-image"/><div id="bl-mobile-img"></div><div id="bl-submitted-content-wrapper"><div id="bl-submitted-title">Awesome, you&rsquo;re all set!</div><small id="bl-submitted-microcopy">We can&rsquo;t wait to make{productName}better with your help. Thank you!</small><button type="button" id="bl-submitted-dismiss">Dismiss</button></div></div><button type="button" id="bl-modal-close"><svg width="24px" height="24px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <polygon id="bl-modal-close-fill" fill="#777777" points="24 22.0631579 22.0631579 24 11.9915789 13.9284211 1.92 24 0 22.0631579 10.0715789 11.9915789 0 1.92 1.92 0 11.9915789 10.0715789 22.0631579 0 24 1.92 13.9284211 11.9915789"></polygon> </g></svg></button></div></div></div>';
+
+// TODO make all content insertable via JS
+// body.insertBefore(modalContent, body.firstChild);
 
 // on smaller screens, adjust some of the UI
 if(narrowWindow) {
 	var movedBtn = blModalCloseBtn.parentNode.removeChild(blModalCloseBtn),
-		movedError = blError;
+		movedError = blError,
+		movedBackLink = blLearnMoreBackButton.parentNode.removeChild(blLearnMoreBackButton);
 
 	movedError.parentNode.removeChild(movedError);
 	blEmail.parentNode.appendChild(movedError);
 	blModal.appendChild(movedBtn);
+	blLearnMoreContent.appendChild(movedBackLink);
 }
 
 function validateEmail(email) { 
@@ -97,6 +113,7 @@ function closeAndResetModal() {
 	blIntroContent.className = '';
 	blSubittedContent.className = '';
 	blModalCloseBtn.className = '';
+	blLearnMoreContent.className = '';
 	body.className = ''; // rudimentary, just to start
 	body.style.height = '';
 	if(narrowWindow) {
@@ -108,16 +125,48 @@ function removeUIAfterSubmit() {
 	var blTitle = blTitleLink.parentNode;
 	blTitle.parentNode.removeChild(blTitle);
 }
+function showLearnMore() {
+	blIntroContent.className = 'is-fading';
+	setTimeout(function() {
+		blIntroContent.className = 'is-hidden';
+		blLearnMoreContent.className = 'is-shown';
+	}, 250);
+	setTimeout(function() {
+		blLearnMoreContent.className += ' is-fading';
+		if(narrowWindow) blLearnMoreVideo.style.height = (window.innerHeight - blLearnMoreBackButton.clientHeight) + 'px';
+	}, 500);
+}
+function hideLearnMore() {
+	blLearnMoreContent.className = 'is-shown';
+	setTimeout(function() {
+		blLearnMoreContent.className = '';
+		blIntroContent.className = '';
+	}, 250);
+}
 
+// show modal
 blTitleLink.onclick = function(e) {
 	e.preventDefault();
 	if(narrowWindow) scrollPos = window.scrollY;
 	showModal();
 	// TODO: app will need to have native URL on site to support users with JS disabled
 }
+
+// show learn more
+blLearnMoreLink.onclick = function() {
+	showLearnMore();
+}
+// hide learn more
+blLearnMoreBackButton.onclick = function() {
+	hideLearnMore();
+}
+
+// close modal
 blModalCloseBtn.onclick = function() {
 	closeAndResetModal();
 }
+
+// email input
 blEmail.onfocus = function() {
 	if(this.value !== '') {
 		this.className += ' ' + hasContentClass;
@@ -133,6 +182,8 @@ blEmail.onblur = function() {
 		showEmailSuccess();
 	}
 }
+
+// form submit
 blForm.onsubmit = function(e) {
 	e.preventDefault();
 	if(!validateEmail(blEmail.value)) {
